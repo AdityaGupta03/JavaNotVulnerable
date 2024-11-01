1f78b7538f4afa615a73ed96e1f87b455851af76
Aditya Gupta <adgupta1003@gmail.com>
2024-11-01 04:41:05 -0400

    Hello World...
    Signed-off-by: Aditya Gupta <adgupta1003@gmail.com>

---
+++ src/main/java/com/example/NotVulnerable.java
@@ -0,0 +1,50 @@
+// src/main/java/com/example/HelloWorld.java
+package com.example;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class NotVulnerable {
+
+  public static void main(String[] args) {
+    System.out.println("Hello, World!");
+    System.out.println("Hello, World!");
+    System.out.println("Hello, World!");
+    System.out.println("Hello, World!");
+    System.out.println("Hello, World!");
+    System.out.println("Hello, World!");
+    System.out.println("Hello, World!");
+    System.out.println("Hello, World!");
+    System.out.println("Hello, World!");
+    System.out.println("Hello, World!");
+    System.out.println("Hello, World!");
+    System.out.println("Hello, World!");
+    System.out.println("Hello, World!");
+    System.out.println("Hello, World!");
+    System.out.println("Hello, World!");
+    System.out.println("Hello, World!");
+    System.out.println("Hello, World!");
+    System.out.println("Hello, World!");
+    System.out.println("Hello, World!");
+    System.out.println("Hello, World!");
+    System.out.println("Hello, World!");
+    System.out.println("Hello, World!");
+    System.out.println("Hello, World!");
+    System.out.println("Hello, World!");
+    System.out.println("Hello, World!");
+    System.out.println("Hello, World!");
+    System.out.println("Hello, World!");
+    System.out.println("Hello, World!");
+    System.out.println("Hello, World!");
+    System.out.println("Hello, World!");
+    System.out.println("Hello, World!");
+    System.out.println("Hello, World!");
+    System.out.println("Hello, World!");
+    System.out.println("Hello, World!");
+    System.out.println("Hello, World!");
+    System.out.println("Hello, World!");
+    System.out.println("Hello, World!");
+    System.out.println("This is my Java project!");
+  }
+}
