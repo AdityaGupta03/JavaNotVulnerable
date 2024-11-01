6b33759d51f821d993c75d1042faa68e0ebe7688
Aditya Gupta <30964439+AdityaGupta03@users.noreply.github.com>
2024-11-01 06:42:39 -0400

    Update NotVulnerable.java

--- src/main/java/com/example/NotVulnerable.java
+++ src/main/java/com/example/NotVulnerable.java
@@ -6,42 +6,6 @@ public class NotVulnerable {
   public static void main(String[] args) {
     // This is a comment
     System.out.println("Hello, World!");
-    System.out.println("Hello, World!");
-    System.out.println("Hello, World!");
-    System.out.println("Hello, World!");
-    System.out.println("Hello, World!");
-    System.out.println("Hello, World!");
-    System.out.println("Hello, World!");
-    System.out.println("Hello, World!");
-    System.out.println("Hello, World!");
-    System.out.println("Hello, World!");
-    System.out.println("Hello, World!");
-    System.out.println("Hello, World!");
-    System.out.println("Hello, World!");
-    System.out.println("Hello, World!");
-    System.out.println("Hello, World!");
-    System.out.println("Hello, World!");
-    System.out.println("Hello, World!");
-    System.out.println("Hello, World!");
-    System.out.println("Hello, World!");
-    System.out.println("Hello, World!");
-    System.out.println("Hello, World!");
-    System.out.println("Hello, World!");
-    System.out.println("Hello, World!");
-    System.out.println("Hello, World!");
-    System.out.println("Hello, World!");
-    System.out.println("Hello, World!");
-    System.out.println("Hello, World!");
-    System.out.println("Hello, World!");
-    System.out.println("Hello, World!");
-    System.out.println("Hello, World!");
-    System.out.println("Hello, World!");
-    System.out.println("Hello, World!");
-    System.out.println("Hello, World!");
-    System.out.println("Hello, World!");
-    System.out.println("Hello, World!");
-    System.out.println("Hello, World!");
-    System.out.println("Hello, World!");
     System.out.println("This is my Java project!");
   }
 }
