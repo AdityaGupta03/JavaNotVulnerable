51f067efc0992836f5940b595fac2f31e31ef41d
Aditya Gupta <adgupta1003@gmail.com>
2024-11-01 05:57:49 -0400

    Removed imports.
    Signed-off-by: Aditya Gupta <adgupta1003@gmail.com>

--- src/main/java/com/example/NotVulnerable.java
+++ src/main/java/com/example/NotVulnerable.java
@@ -1,10 +1,6 @@
 // src/main/java/com/example/HelloWorld.java
 package com.example;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-
 public class NotVulnerable {
 
   public static void main(String[] args) {
